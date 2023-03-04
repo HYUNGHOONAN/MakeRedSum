98f92939c0233f1532308e0c6b153bbb392af13c
안형훈 <>
2023-03-04 20:57:15 +0900

    푸시 테스트 2

--- app/src/main/java/com/example/makeredsum/MainActivity.kt
+++ app/src/main/java/com/example/makeredsum/MainActivity.kt
@@ -12,5 +12,6 @@ class MainActivity : AppCompatActivity() {
     // 알림 기능이 추가 됐습니다.
     // 충돌시킬 텍스트추가
     // 충돌시킬 브랜치 충돌 시킬 텍스트를 추가한다.
-    // 푸시 관련된 내용
+    // 푸시 관련된 내용 - commit 1
+    // 푸시 관련된 내용 - commit 2
 }
